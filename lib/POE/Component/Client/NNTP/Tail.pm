912d4d343d7cc6a845b492fc131bbee0dd1c13c8
David Golden <dagolden@cpan.org>
2011-08-18 18:46:43 -0400

    Converted distribution to dzil

--- lib/POE/Component/Client/NNTP/Tail.pm
+++ lib/POE/Component/Client/NNTP/Tail.pm
@@ -1,16 +1,9 @@
-# Copyright (c) 2008 by David Golden. All rights reserved.
-# Licensed under Apache License, Version 2.0 (the "License").
-# You may not use this file except in compliance with the License.
-# A copy of the License was distributed with this file or you may obtain a
-# copy of the License from http://www.apache.org/licenses/LICENSE-2.0
-
 package POE::Component::Client::NNTP::Tail;
 use 5.006;
 use strict;
 use warnings;
-
-our $VERSION = '0.01';
-$VERSION = eval $VERSION; ## no critic
+# ABSTRACT: Sends events for new articles posted to an NNTP newsgroup
+# VERSION
 
 use Carp::POE;
 use Params::Validate;
@@ -326,15 +319,9 @@ sub _nntp_got_article {
 
 __END__
 
+=pod
+
 =begin wikidoc
-
-= NAME
-
-POE::Component::Client::NNTP::Tail - Sends events for new articles posted to an NNTP newsgroup
-
-= VERSION
-
-This documentation describes version %%VERSION%%.
 
 = SYNOPSIS
 
@@ -494,32 +481,6 @@ existing test-file that illustrates the bug or desired feature.
 
 * [POE]
 * [POE::Component::Client::NNTP]
-
-= AUTHOR
-
-David A. Golden (DAGOLDEN)
-
-Portions based on or inspired by code in
-POE::Component::SmokeBox::Uploads::NNTP by Chris Williams.
-
-= COPYRIGHT AND LICENSE
-
-Copyright (c) 2008 by David A. Golden. All rights reserved.
-
-Licensed under Apache License, Version 2.0 (the "License").
-You may not use this file except in compliance with the License.
-A copy of the License was distributed with this file or you may obtain a
-copy of the License from http://www.apache.org/licenses/LICENSE-2.0
-
-Files produced as output though the use of this software, shall not be
-considered Derivative Works, but shall be considered the original work of the
-Licensor.
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
 
 =end wikidoc
 
